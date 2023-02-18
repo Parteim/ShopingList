6a0ad2001c805e0317dc4b823fcdbdcc64a352dd
Parteim <Sergeykaynov@gmail.com>
2023-02-18 19:10:44 +0700

    Implementation ShopListRepository.kt in data layer

--- app/src/main/java/com/example/shopinglist/domain/ShopListRepository.kt
+++ app/src/main/java/com/example/shopinglist/domain/ShopListRepository.kt
@@ -3,8 +3,8 @@ package com.example.shopinglist.domain
 interface ShopListRepository {
     fun addShopItem(shopItem: ShopItem)
 
-    fun deleteShopItem(ShopItem: ShopItem)
-    fun editShopItem(ShopItem: ShopItem)
+    fun deleteShopItem(shopItem: ShopItem)
+    fun editShopItem(shopItem: ShopItem)
     fun getShopItem(id: Int): ShopItem
     fun getShopList(): List<ShopItem>
 }
